20b4eb85043760c0ecf3d0acf4d9b1ec7cff5293
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-04-14 09:07:52 +0530

    create header components

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,17 @@
+// eslint-disable-next-line no-undef
+const flowbite = require("flowbite-react/tailwind")
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+        flowbite.content(),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [flowbite.plugin()],
 }
